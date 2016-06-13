3157968a077449c46cf774b4b11a9ee9be5f5cd2
rmoj <rmoj99@gmail.com>
2016-06-12 21:03:56 -0400

    first commit

---
+++ fizzbuzz.c
@@ -0,0 +1,27 @@
+/*
+ FizzBuzz.c
+ Rene Mojica
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i=1; i<101; i++){
+        if ((i%3)==0) {
+            if ((i%5)==0) {
+                printf("FizzBuzz\n");
+            }  else {
+                printf("Fizz\n");
+            }
+            
+        }  else {
+            if ((i%5)==0) {
+                printf("Buzz\n");
+            }  else {
+                printf("%d\n",i);
+            }
+        }			
+    }
+    return 0;
+}
